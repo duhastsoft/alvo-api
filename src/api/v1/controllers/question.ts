b3683258f1abc9fcfb18d0e4fa41aaa13d413e12
TheAlexBig <00062816@uca.edu.sv>
2020-10-01 11:07:52 -0600

    adding flavor

--- src/api/v1/controllers/question.ts
+++ src/api/v1/controllers/question.ts
@@ -4,12 +4,12 @@ async function obtainAll(req: Request, res: Response) {
     res.status(200).send('Question done');
   }
 
-  async function remove(req: Request, res: Response) {
+async function remove(req: Request, res: Response) {
     res.status(200).send('Question record deleted');
-  }
+}
 
-  async function update(req: Request, res: Response) {
+async function update(req: Request, res: Response) {
     res.status(200).send('Question record updated');
-  }
+}
 
 export {obtainAll, remove, update};
--- src/api/v1/routes/user.ts
+++ src/api/v1/routes/user.ts
@@ -3,7 +3,10 @@ import * as controller from '../controllers/user';
 
 const router = express.Router();
 
+//GET ENDPOINTS
 router.get('/', controller.obtainAll);
+//POST ENDPOINTS
 router.post('/login', controller.login);
+router.post('/logout', controller.logout);
 
 export default router;

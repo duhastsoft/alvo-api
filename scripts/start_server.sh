fb331a083aea992cf28b4c29c8923e31011a5d57
TheAlexBig <00062816@uca.edu.sv>
2020-10-01 19:14:28 -0600

    better scripts and yml

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 cd /home/linux/
+export PM2_HOME=/home/linux/.pm2 
 
 isExistApp = `pgrep httpd`
 if [[ -n  $isExistApp ]]; then
     service httpd stop
 fi
-export PM2_HOME=/home/linux/.pm2  
+ 
 yum remove -y httpd
-
 pm2 delete all
+
 pm2 start server.js

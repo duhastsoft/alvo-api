ede94a1b2ffecfb62c0a5947f057deadfb76f04b
TheAlexBig <00062816@uca.edu.sv>
2020-10-01 19:28:45 -0600

    legacy variable declaration removed

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 cd /home/linux/
-export PM2_HOME=/home/linux/.pm2 
-
-isExistApp = `pgrep httpd`
+isExistApp=$(pgrep httpd)
 if [[ -n  $isExistApp ]]; then
     service httpd stop
 fi
- 
 yum remove -y httpd
+export PM2_HOME=/home/linux/.pm2 
 pm2 delete all
 
 pm2 start server.js

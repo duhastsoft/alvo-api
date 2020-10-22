a4e1f0d65180cddbb29e6d6c5ac6705be8684bac
TheAlexBig <00062816@uca.edu.sv>
2020-10-22 17:02:32 -0600

    Consistency in functions and code

--- src/api/v1/middlewares/access-verification.ts
+++ src/api/v1/middlewares/access-verification.ts
@@ -3,35 +3,29 @@ import { getManager } from 'typeorm';
 import Role from '../entity/Role';
 
 export const accessVerify =  (allowed: string, entity: string) => {
-  return (req: Request, res: Response, next: NextFunction): void => {
+  return async function (req: Request, res: Response, next: NextFunction): Promise<void>  {
     const roleRepository = getManager().getRepository<Role>(Role);
-    roleRepository.findOne({
-      where: { name: req.userData.role },
-      relations: ['permisions'],
-    }).then(foundRole=>{
-      if (foundRole) {
-        const foundPermision = foundRole.permisions.find((e) => {
-          return e.name == entity;
-        });
-        if (foundPermision && foundPermision.actions.find((e) => e == allowed)) {
-          next();
-        }
-        else{
-          res.status(401).json({
-            message: 'Your lack of permissions prevents you from accessing this route - Permission',
-          });
-        }
+    try{
+      const foundRole = await roleRepository.findOneOrFail({
+        where: { name: req.userData.role },
+        relations: ['permisions'],
+      });
+      const foundPermision = foundRole.permisions.find((e) => {
+        return e.name == entity;
+      });
+      if (foundPermision && foundPermision.actions.find((e) => e == allowed)) {
+        next();
       }
       else{
         res.status(401).json({
-          message: 'Your lack of permissions prevents you from accessing this route - Role',
+          message: 'Your lack of permissions prevents you from accessing this route',
         });
       }
-    }).catch(err=>{
-      res.status(500).json({
-        message: 'Unsuccessful search of Role',
-      });
-    });
+    }
+    catch(err){
+      if (err.constructor.name === 'EntityNotFoundError') res.status(404);
+      next(err);
+    }
   };
 };
 

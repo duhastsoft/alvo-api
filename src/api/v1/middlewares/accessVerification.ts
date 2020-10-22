c14a5c4ef8951a7d545a119817a4ab88d4bb4892
TheAlexBig <00062816@uca.edu.sv>
2020-10-22 09:30:30 -0600

    Cleaning access verification middleware

--- src/api/v1/middlewares/accessVerification.ts
+++ src/api/v1/middlewares/accessVerification.ts
@@ -2,25 +2,35 @@ import { NextFunction, Request, Response } from 'express';
 import { getManager } from 'typeorm';
 import Role from '../entity/Role';
 
-const accessVerify = function (allowed: string, entity: string) {
-  const targetAllowed = allowed;
-  const targetEntity = entity;
-  return async function (req: Request, res: Response, next: NextFunction) {
+export const accessVerify =  (allowed: string, entity: string) => {
+  return (req: Request, res: Response, next: NextFunction): void => {
     const roleRepository = getManager().getRepository<Role>(Role);
-    const foundRole = await roleRepository.findOne({
+    roleRepository.findOne({
       where: { name: req.userData.role },
       relations: ['permisions'],
-    });
-    if (foundRole) {
-      const foundPermision = foundRole.permisions.find((e) => {
-        return e.name == targetEntity;
-      });
-      if (foundPermision && foundPermision.actions.find((e) => e == targetAllowed)) {
-        return next();
+    }).then(foundRole=>{
+      if (foundRole) {
+        const foundPermision = foundRole.permisions.find((e) => {
+          return e.name == entity;
+        });
+        if (foundPermision && foundPermision.actions.find((e) => e == allowed)) {
+          next();
+        }
+        else{
+          res.status(401).json({
+            message: 'Your lack of permissions prevents you from accessing this route - Permission',
+          });
+        }
       }
-    }
-    return res.status(401).json({
-      message: 'Your lack of permissions prevents you from accessing this route',
+      else{
+        res.status(401).json({
+          message: 'Your lack of permissions prevents you from accessing this route - Role',
+        });
+      }
+    }).catch(err=>{
+      res.status(500).json({
+        message: 'Unsuccessful search of Role',
+      });
     });
   };
 };

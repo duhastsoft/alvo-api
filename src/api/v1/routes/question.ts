a6c43683eee20797071534fe10f6afb0c24a70e8
Kevin Velasquez <kevin.vela519@gmail.com>
2020-11-10 22:37:30 -0600

    Add authorization to existing routes

--- src/api/v1/routes/question.ts
+++ src/api/v1/routes/question.ts
@@ -1,26 +1,49 @@
 import express from 'express';
 import controller from '../controllers/question';
+import { UserRole } from '../entity/User';
+import authenticate from '../middlewares/authentication';
+import { authorize } from '../middlewares/authorization';
 import * as schemas from '../validation/question-schemas';
 import { validateBody, validateParams } from '../validation/validation';
 
 const router = express.Router();
 
 // GET ENDPOINTS
-router.get('/', controller.obtainAll);
-router.get('/:id', validateParams(schemas.paramId), controller.findById);
+router.get('/', authenticate, authorize([UserRole.Admin]), controller.obtainAll);
+router.get(
+  '/:id',
+  authenticate,
+  authorize([UserRole.Admin]),
+  validateParams(schemas.paramId),
+  controller.findById
+);
 
 // POST ENDPOINTS
-router.post('/', validateBody(schemas.create), controller.create);
+router.post(
+  '/',
+  authenticate,
+  authorize([UserRole.Admin]),
+  validateBody(schemas.create),
+  controller.create
+);
 
 // PUT ENDPOINTS
 router.put(
   '/:id',
+  authenticate,
+  authorize([UserRole.Admin]),
   validateParams(schemas.paramId),
   validateBody(schemas.create),
   controller.update
 );
 
 // DELETE ENDPOINTS
-router.delete('/:id', validateParams(schemas.paramId), controller.remove);
+router.delete(
+  '/:id',
+  authenticate,
+  authorize([UserRole.Admin]),
+  validateParams(schemas.paramId),
+  controller.remove
+);
 
 export default router;
--- src/api/v1/routes/service-category.ts
+++ src/api/v1/routes/service-category.ts
@@ -1,5 +1,8 @@
 import express from 'express';
 import controller from '../controllers/service-category';
+import { UserRole } from '../entity/User';
+import authenticate from '../middlewares/authentication';
+import { authorize } from '../middlewares/authorization';
 import * as schemas from '../validation/service-category-schemas';
 import { validateBody, validateParams } from '../validation/validation';
 
@@ -8,21 +11,34 @@ const router = express.Router();
 // GET ENDPOINTS
 router.get('/all', controller.obtainAllCategories);
 router.get('/expanded', controller.obtainAllServices);
-router.get('/:id',  validateParams(schemas.paramId),  controller.findById);
+router.get('/:id', validateParams(schemas.paramId), controller.findById);
 
 // POST ENDPOINTS
-router.post('/', validateBody(schemas.create), controller.create);
+router.post(
+  '/',
+  authenticate,
+  authorize([UserRole.Admin]),
+  validateBody(schemas.create),
+  controller.create
+);
 
 // PUT ENDPOINTS
 router.put(
-    '/:id',
-    validateParams(schemas.paramId),
-    validateBody(schemas.create),
-    controller.update
-  );
-  
-  // DELETE ENDPOINTS
-  router.delete('/:id', validateParams(schemas.paramId), controller.remove);
+  '/:id',
+  authenticate,
+  authorize([UserRole.Admin]),
+  validateParams(schemas.paramId),
+  validateBody(schemas.create),
+  controller.update
+);
 
+// DELETE ENDPOINTS
+router.delete(
+  '/:id',
+  authenticate,
+  authorize([UserRole.Admin]),
+  validateParams(schemas.paramId),
+  controller.remove
+);
 
 export default router;

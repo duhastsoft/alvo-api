0175ab7587998b8f2770001e408509dd9aef6df9
Kevin Velasquez <kevin.vela519@gmail.com>
2020-11-12 23:25:35 -0600

    Add endpoint to submit exam
    * Added function in controller to save an exam
    * Added route in quiz to submit exam
    * Updated userDate structure

--- src/api/v1/routes/quiz.ts
+++ src/api/v1/routes/quiz.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import * as controller from '../controllers/quiz';
+import authenticate from '../middlewares/authentication';
 import { testQuerySchema } from '../validation/quiz-schema';
 import { validateQuery } from '../validation/validation';
 
@@ -8,5 +9,6 @@ const router = Router();
 router.get('/category', validateQuery(testQuerySchema), controller.getCategoryQuiz);
 router.get('/vmt', controller.getVmtQuiz);
 router.get('/free', validateQuery(testQuerySchema), controller.getFreeQuiz);
+router.post('/', authenticate, controller.submitExam);
 
 export default router;
--- src/api/v1/tools/custom.d.ts
+++ src/api/v1/tools/custom.d.ts
@@ -1,8 +1,8 @@
 declare namespace Express {
-    export interface Request {
-       userData: {
-           id?: string;
-           role? : string;
-       }
-    }
+  export interface Request {
+    userData: {
+      id: string;
+      role: string;
+    };
+  }
 }
